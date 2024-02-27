e23aabb0904f13384396a7d8d4574015b34fe2f8
Arman Galstyan <armangalstyan9597@gmail.com>
2024-02-27 16:52:17 +0400

    Add Leaf tests, add documentation

--- driven_layout/lib/src/template/language/templates/leaf.dart
+++ driven_layout/lib/src/template/language/templates/leaf.dart
@@ -1,5 +1,22 @@
+import 'package:driven_layout/src/template/dispatcher/dispatcher.dart' show Dispatcher;
+import 'package:driven_layout/src/template/language/ast_entities/ast_entities.dart' show AstEntity;
+import 'package:driven_layout/src/template/language/ast_entities/node_types.dart' show Token;
+import 'package:driven_layout/src/template/language/templates/templates.dart' show Template;
+
 import 'unit.dart';
 
+/// Leaf node of [Template] tree
+/// 
+/// Cooridnates [Dispatcher] to expect [Token] in [AstEntity] tree with given [value].
+/// 
+/// ```dart
+/// final exampleNode = Node(
+///   ClassDeclaration,
+///   [
+///     Leaf("class"),
+///   ],
+/// );
+/// ```
 class Leaf extends Unit {
   final String value;
 
---
+++ driven_layout/test/template/language/templates/leaf_test.dart
@@ -0,0 +1,8 @@
+import 'package:driven_layout/src/template/language/templates/templates.dart' show Leaf;
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<Leaf>()])
+
+void main() { 
+  // No tests. Only for mock generation.
+}
